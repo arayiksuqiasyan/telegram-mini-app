2a329ef1eba00c3af0d644d2348a07f16f909c47
Arayik <arayiksuqiasyan20@gmail.com>
2024-12-12 21:22:05 +0400

    configure ton

--- next.config.mjs
+++ next.config.mjs
@@ -5,11 +5,15 @@ const nextConfig = {
     domains: [],
   },
   reactStrictMode: false,
+  env: {
+    NEXT_PUBLIC_MANIFEST_URL:
+      'https://ivory-large-asp-773.mypinata.cloud/files/bafkreicdgwqarnageonctkh36u6sv6mxkpbwhsoonoppfczlkk3ucrwpdq?X-Algorithm=PINATA1&X-Date=1734021142&X-Expires=30&X-Method=GET&X-Signature=b09687985c09d68f19017c969e89f49f6c6d115c8b06eee4f614da5750a68eb9',
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"],
+      use: ['@svgr/webpack'],
     })
 
     return config
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,11 @@
 'use client'
 import React, { PropsWithChildren } from 'react'
 import './app.scss'
-import dynamic from "next/dynamic";
+import dynamic from 'next/dynamic'
 
-const TonConnectUIProvider = dynamic(
-  () => import('@tonconnect/ui-react').then(mod => mod.TonConnectUIProvider),
-  { ssr: false }
-);
-
+const TonConnectUIProvider = dynamic(() => import('@tonconnect/ui-react').then(mod => mod.TonConnectUIProvider), {
+  ssr: false,
+})
 
 interface IRootLayout extends PropsWithChildren {
   children?: React.ReactNode
@@ -17,7 +15,7 @@ const RootLayout: React.FC<IRootLayout> = ({ children }) => {
   return (
     <html lang="en">
       <body>
-        <TonConnectUIProvider manifestUrl={"https://ivory-large-asp-773.mypinata.cloud/files/bafkreicdgwqarnageonctkh36u6sv6mxkpbwhsoonoppfczlkk3ucrwpdq?X-Algorithm=PINATA1&X-Date=1734021142&X-Expires=30&X-Method=GET&X-Signature=b09687985c09d68f19017c969e89f49f6c6d115c8b06eee4f614da5750a68eb9"}>{children}</TonConnectUIProvider>
+        <TonConnectUIProvider manifestUrl={process.env.NEXT_PUBLIC_MANIFEST_URL}>{children}</TonConnectUIProvider>
       </body>
     </html>
   )
