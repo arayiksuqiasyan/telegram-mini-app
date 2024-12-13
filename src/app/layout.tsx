afe88926906afb00fa77bbd3a9f5fe6ce0e555d4
Arayik <arayiksuqiasyan20@gmail.com>
2024-12-13 16:30:30 +0400

    fix manifest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,9 @@ const RootLayout: React.FC<IRootLayout> = ({ children }) => {
   return (
     <html lang="en">
       <body>
-        <TonConnectUIProvider manifestUrl={process.env.NEXT_PUBLIC_MANIFEST_URL}>{children}</TonConnectUIProvider>
+        <TonConnectUIProvider manifestUrl={"https://ivory-large-asp-773.mypinata.cloud/files/bafkreiedjcvw3xbsu4kivs6optmubztrt5tytiqi37hkfxdecx4spw4t2u?X-Algorithm=PINATA1&X-Date=1734092985&X-Expires=30&X-Method=GET&X-Signature=31ce2833613c84c714c294414d2afc632ff46550e779b0cbe8d1715bcbe5d8d6"}>
+          {children}
+        </TonConnectUIProvider>
       </body>
     </html>
   )
