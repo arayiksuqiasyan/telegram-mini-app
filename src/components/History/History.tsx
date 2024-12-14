6b49b24ee4dd15110e1f1dee6c80a2da8340afff
Arayik <arayiksuqiasyan20@gmail.com>
2024-12-14 18:00:00 +0400

    changes

---
+++ src/components/History/History.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import classes from './History.module.scss'
+
+
+const History = () => {
+  return (
+    <div>
+      <span>History</span>
+    </div>
+  )
+}
+
+export default History
