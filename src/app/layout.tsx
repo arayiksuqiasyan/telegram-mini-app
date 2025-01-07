c249417ee344ce235ef9bde8a44f84e7b3a9b892
Arayik <arayiksuqiasyan20@gmail.com>
2025-01-07 15:07:15 +0400

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,7 +58,7 @@ const RootLayout: React.FC<IRootLayout> = ({ children }) => {
           <Button
             onClick={() => {
               if (typeof window !== 'undefined') {
-                WebApp.shareMessage('0579XN84SbXbIyhf', param => {
+                WebApp.shareMessage('ENJYnmU2pUhIdvuR', param => {
                   setStatus(String(param))
                 })
 
